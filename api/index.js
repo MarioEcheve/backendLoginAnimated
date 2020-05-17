42817e31ac6584b629f8b62a459f4af0796a50fa
marioandreseche@gmail.com <marioecheverrialopez@MacBook-Pro-de-Mario.local>
2020-05-17 18:30:59 -0400

    integracion mongoose

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = requiere("mongoose");
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.get("*", (req, res) => {
   console.log("hola mundo");
