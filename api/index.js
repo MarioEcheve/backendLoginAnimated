0e94efa2f8314c3fedc881328f22605a2e29011d
marioandreseche@gmail.com <marioecheverrialopez@MacBook-Pro-de-Mario.local>
2020-05-17 17:43:00 -0400

    first commit

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("*", (req, res) => {
+  console.log("hola mundo");
+  res.send({ mensaje: " chancho feliz " });
+});
+
+module.exports = app;
