95df3de9f36878ab2dd678cfaa4ca2e97c6e1a00
marioandreseche@gmail.com <marioecheverrialopez@MacBook-Pro-de-Mario.local>
2020-05-17 18:43:37 -0400

    conexion a mongo

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = requiere("mongoose");
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const Users = mongoose.model("User", new mongoose.Schema({ name: String }));
 Users.create({
   name: "Mario",
